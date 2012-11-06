6e9e0a1bb283690ccc061c202492ce518e71dc63
dawidh <david.enochs@gmail.com>
2012-11-06 16:45:36 -0500

    URL friendly folder name
    Removed spaces form paths

--- PR1/layout.php
+++ PR1/layout.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!--[CSSE 4440] Term Project-->
 		<meta charset="utf-8" />
-
-		<title>contact</title>
+		<title>[CSSE 4440] Term Project</title>
 		<meta name="description" content="" />
 		<meta name="author" content="Glenn Bishop, David Enochs, Ian Walker" />
 
@@ -20,12 +20,12 @@
 				<h1>Don Durma's Ice Cream Parlor</h1>
 			</header>
 			<nav>
-					<a href="/[CSSE%204440]%20Term%20Project/PR1/layout.php?page=welcome">Home</a>
-					<a href="/[CSSE%204440]%20Term%20Project/PR1/layout.php?page=location">Location</a>
-					<a href="/[CSSE%204440]%20Term%20Project/PR1/layout.php?page=specials">Specials</a>
-					<a href="/[CSSE%204440]%20Term%20Project/PR1/layout.php?page=menu">Menu</a>
-					<a href="/[CSSE%204440]%20Term%20Project/PR1/layout.php?page=reservations">Reservations</a>
-					<a href="/[CSSE%204440]%20Term%20Project/PR1/layout.php?page=contact">Contact</a>
+					<a href="/CSSE4440TermProject/PR1/layout.php?page=welcome">Home</a>
+					<a href="/CSSE4440TermProject/PR1/layout.php?page=location">Location</a>
+					<a href="/CSSE4440TermProject/PR1/layout.php?page=specials">Specials</a>
+					<a href="/CSSE4440TermProject/PR1/layout.php?page=menu">Menu</a>
+					<a href="/CSSE4440TermProject/PR1/layout.php?page=reservations">Reservations</a>
+					<a href="/CSSE4440TermProject/PR1/layout.php?page=contact">Contact</a>
 			</nav>
 
 			<div>
